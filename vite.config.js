d5a878f9fb6a54bc235783c98ba6531f3f7d7791
rmkane <rmkane89@gmail.com>
2024-02-06 20:33:57 -0500

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,12 @@ import { defineConfig } from "vite";
 
 const __dirname = import.meta.dirname;
 
-const LIB_NAME = "keyboard-js";
-
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "lib/main.ts"),
-      name: LIB_NAME,
-      fileName: (format) => `${LIB_NAME}.${format}.js`,
+      name: "KeyboardJS",
+      fileName: (format) => `keyboard-js.${format}.js`,
     },
   },
 });
