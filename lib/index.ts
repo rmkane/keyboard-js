8ec8af17d2845379b1002be52ab8c9ac5a7b88df
rmkane <rmkane89@gmail.com>
2024-02-07 15:26:17 -0500

    Begin styling

--- lib/index.ts
+++ lib/index.ts
@@ -13,8 +13,8 @@ type LayoutData = {
   };
   metadata: {
     display: "grid" | "flex";
-    rows: number;
-    cols: number;
+    rows?: number;
+    cols?: number;
   };
 };
 
@@ -23,8 +23,6 @@ const keyMap = (keys as KeyData[]).reduce(
   new Map<number, KeyValue>()
 );
 
-console.log(fullLayout);
-
 type OperatingSystemType = "mac" | "windows";
 type KeyboardType = "full" | "tkl";
 
@@ -32,6 +30,20 @@ type LayoutOptions = {
   keyboardType?: KeyboardType;
   os?: OperatingSystemType;
 };
+
+function isNumberArray(value: any): value is number[] {
+  return (
+    Array.isArray(value) && value.every((item) => typeof item === "number")
+  );
+}
+
+function isNumberArray2D(value: any): value is number[][] {
+  return Array.isArray(value) && value.every(isNumberArray);
+}
+
+function isNumberArray3D(value: any): value is number[][][] {
+  return Array.isArray(value) && value.every(isNumberArray2D);
+}
 
 const defaultOptions: LayoutOptions = {
   keyboardType: "full",
@@ -46,19 +58,68 @@ function initializeLayout(
 
   const opts: LayoutOptions = { ...defaultOptions, ...options };
 
-  for (let keyboardElement of keyboardElements) {
-    keyboardElement.replaceChildren();
-
-    for (let i = 0; i < 10; i++) {
-      const keyElement: HTMLDivElement = document.createElement("div");
-      keyElement.classList.add("key");
-      keyElement.textContent = i.toString();
-      keyboardElement.append(keyElement);
-    }
+  for (let keyboardEl of keyboardElements) {
+    renderKeyboard(keyboardEl, opts);
   }
+}
 
-  console.log("Keyboard type:", opts.keyboardType);
-  console.log("OS:", opts.os);
+function renderKeyboard(keyboardEl: HTMLDivElement, opts: LayoutOptions) {
+  keyboardEl.replaceChildren();
+
+  switch (opts.keyboardType) {
+    case "full":
+      return renderFullKeyboard(keyboardEl, opts);
+    case "tkl":
+      return renderTklKeyboard(keyboardEl, opts);
+  }
+}
+
+function renderFullKeyboard(keyboardEl: HTMLDivElement, opts: LayoutOptions) {
+  (fullLayout as LayoutData[]).forEach(({ data, metadata }) => {
+    const regionEl = document.createElement("div");
+    regionEl.dataset.region = data.region;
+    regionEl.style.display = metadata.display;
+
+    if (regionEl.style.display === "grid") {
+      regionEl.style.gridTemplateColumns = `repeat(${metadata.cols}, auto)`;
+      (data.keys as number[]).forEach((which) => {
+        regionEl.append(renderKey(which, opts));
+      });
+    } else if (regionEl.style.display === "flex") {
+      if (isNumberArray(data.keys)) {
+        (data.keys as number[]).forEach((which) => {
+          regionEl.append(renderKey(which, opts));
+        });
+      } else if (isNumberArray2D(data.keys)) {
+        (data.keys as number[][]).forEach((row) => {
+          const rowEl = document.createElement("div");
+          row.forEach((which) => {
+            rowEl.append(renderKey(which, opts));
+          });
+          regionEl.append(rowEl);
+        });
+      } else {
+        throw new Error("Unsupported n-dimensional array");
+      }
+    }
+    keyboardEl.append(regionEl);
+  });
+}
+
+function renderKey(which: number, opts: LayoutOptions) {
+  const keyElement: HTMLDivElement = document.createElement("div");
+  keyElement.classList.add("key");
+  keyElement.textContent = which.toString();
+  return keyElement;
+}
+
+function renderTklKeyboard(keyboardEl: HTMLDivElement, opts: LayoutOptions) {
+  for (let i = 0; i < 10; i++) {
+    const keyElement: HTMLDivElement = document.createElement("div");
+    keyElement.classList.add("key");
+    keyElement.textContent = i.toString();
+    keyboardEl.append(keyElement);
+  }
 }
 
 export { initializeLayout };
